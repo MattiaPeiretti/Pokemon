b834650683e01e838d3a3c82433ccb8139659778
Mattia Peiretti <mattia.peiretti@gmail.com>
2022-04-10 15:50:13 +0200

    Created dashboard structure

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHashHistory } from "vue-router";
-import dashboard from "../views/dashboard.vue";
+import dashboard from "../views/DashboardView.vue";
 
 const routes = [
     {
@@ -7,14 +7,14 @@ const routes = [
         name: "Dashboard",
         component: dashboard,
     },
-    {
-        path: "/about",
-        name: "about",
-        // route level code-splitting
-        // this generates a separate chunk (about.[hash].js) for this route
-        // which is lazy-loaded when the route is visited.
-        component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-    },
+    // {
+    //     path: "/about",
+    //     name: "about",
+    //     // route level code-splitting
+    //     // this generates a separate chunk (about.[hash].js) for this route
+    //     // which is lazy-loaded when the route is visited.
+    //     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    // },
 ];
 
 const router = createRouter({
---
+++ src/views/DashboardView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container mx-auto flex flex-col items-center">
+    <img src="@/assets/img/pikachu.png" alt="pikachu" />
+    <h1>
+      Look up your favorite <br />
+      pokemon!
+    </h1>
+
+    <div class="flex items-center justify-between">
+      <pokemonSearchbar></pokemonSearchbar>
+      <primaryButton>Search</primaryButton>
+    </div>
+  </div>
+</template>
+
+<script>
+import pokemonSearchbar from "@/components/pokemon-searchbar.vue";
+import primaryButton from "@/components/basic-ui/primary-button.vue";
+
+export default {
+  name: "DashboardView",
+  components: {
+    pokemonSearchbar,
+    primaryButton,
+  },
+};
+</script>
