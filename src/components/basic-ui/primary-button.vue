0385270f2cc88e38c10d2c607d18e35236ef8558
Mattia Peiretti <mattia.peiretti@gmail.com>
2022-04-10 15:57:55 +0200

    Created primary button component

---
+++ src/components/basic-ui/primary-button.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    class="
+      text-white
+      bg-yellow-400
+      py-2
+      px-3
+      ring-black
+      hover:ring-4 hover:text-black
+    "
+  >
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "PrimaryButton",
+};
+</script>
