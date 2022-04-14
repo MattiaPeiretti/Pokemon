5d2788c279eb0acb888eab54a6fa2c835af30662
Mattia Peiretti <mattia.peiretti@gmail.com>
2022-04-14 22:35:47 +0200

    Stepping stones detail view!

--- src/router/index.js
+++ src/router/index.js
@@ -7,14 +7,14 @@ const routes = [
         name: "Dashboard",
         component: dashboard,
     },
-    // {
-    //     path: "/about",
-    //     name: "about",
-    //     // route level code-splitting
-    //     // this generates a separate chunk (about.[hash].js) for this route
-    //     // which is lazy-loaded when the route is visited.
-    //     component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-    // },
+    {
+        path: "/details",
+        name: "details",
+        // route level code-splitting
+        // this generates a separate chunk (about.[hash].js) for this route
+        // which is lazy-loaded when the route is visited.
+        component: () => import(/* webpackChunkName: "about" */ "../views/DetailsView.vue"),
+    },
 ];
 
 const router = createRouter({
