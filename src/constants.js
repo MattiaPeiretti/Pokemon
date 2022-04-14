dc33d3f4ff69d1ff402718920c182e7eee3c6bda
Mattia Peiretti <mattia.peiretti@gmail.com>
2022-04-14 22:34:42 +0200

    Added some constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,7 @@
 export default {
     pokemonRESTAPIBaseURL: "https://pokeapi.co/api/v2/pokemon",
     pokemonGraphqlAPIBaseURL: "https://beta.pokeapi.co/graphql/v1beta",
+    pokemonSpritesBaseURL:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon",
+    pokemonDetailsPageBaseURL: "/details",
 };
